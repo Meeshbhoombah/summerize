05cb00fd07290f95eed6805f18b38a18983b3baf
Rohan Mishra <rohanmishra2017@gmail.com>
2018-02-01 14:43:30 -0800

    Created baseline filestructure for script

---
+++ concatenate.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*- 
+"""
+concatenate.py
+
+Creates MySQL databases and .csv files from given Make School Summer Academy Feedback datasets.
+"""
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
+
